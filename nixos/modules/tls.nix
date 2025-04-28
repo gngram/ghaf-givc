e1bced353ab081802eb5d94268b39084dc107fb7
Ganga Ram <Ganga.Ram@tii.ae>
2025-04-28 15:38:17 +0400

    test
    Signed-off-by: Ganga Ram <Ganga.Ram@tii.ae>

--- nixos/modules/tls.nix
+++ nixos/modules/tls.nix
@@ -77,15 +77,50 @@ in
     systemd.services = {
       givc-key-setup =
         let
+          inherit (cfg)
+            generatorHostName
+            ;
           givcCertGenerator = pkgs.callPackage ../packages/givc-gen-certs.nix {
             inherit lib pkgs;
             inherit (cfg)
               agents
-              adminTlsName
-              adminAddresses
-              generatorHostName
               ;
           };
+          genGivcCerts = pkgs.writeShellScriptBin "gen_givc_mtls_cred" ''
+            set -xeuo pipefail
+            tmp_certs_dir=$(${pkgs.coreutils}/bin/mktemp --directory)
+            ${givcCertGenerator}/bin/gen_mtls_creds "$tmp_certs_dir" givc
+            # Copy host certificates to '/etc/givc' directory
+            [[ -d "/etc/givc" ]] && rm -r "/etc/givc"
+            mkdir -p "/etc/givc"
+            cp -r "$tmp_certs_dir/${cfg.generatorHostName}"/* /etc/givc
+            cp -r "$tmp_certs_dir"/agents /etc/givc/givc.certs
+            cp -r "$tmp_certs_dir"/certification-authority/ca-cert.pem /etc/givc
+            # Function to create image file for agents
+            create_image(){
+              name="$1"
+              image="${cfg.storagePath}/''${name}.img"
+              [[ -f "$image" ]] && rm -r "$image"
+              ${pkgs.coreutils}/bin/truncate -s 2M "$image"
+              ${pkgs.e2fsprogs}/bin/mkfs.ext4 -L "givc-''${name}" "$image"
+              tmpmnt=$(${pkgs.coreutils}/bin/mktemp --directory)
+              ${pkgs.mount}/bin/mount "$image" "$tmpmnt"
+              cp -r "$tmp_certs_dir/$name"/* "$tmpmnt"
+              cp -r "$tmp_certs_dir"/certification-authority/ca-cert.pem "$tmpmnt"
+              ${pkgs.umount}/bin/umount "$tmpmnt"
+              rm -rf "$tmpmnt"
+            }
+            # Generate image of keys/certificates for all agents except host
+            ${lib.concatStringsSep "\n" (
+              map (entry: "create_image ${entry.name}") (
+                lib.filter (agent: agent.name != "${cfg.generatorHostName}") cfg.agents
+              )
+            )}
+            rm -rf "$tmp_certs_dir"
+            # Create lock file
+            ${pkgs.coreutils}/bin/install -m 000 /dev/null /etc/givc/tls.lock
+            /run/current-system/systemd/bin/systemd-notify --ready
+          '';
         in
         {
           enable = true;
@@ -100,7 +135,7 @@ in
             Restart = "no";
             StandardOutput = "journal";
             StandardError = "journal";
-            ExecStart = "${givcCertGenerator}/bin/givc-gen-certs ${cfg.storagePath}";
+            ExecStart = "${genGivcCerts}/bin/gen_givc_mtls_cred";
           };
         };
       givc-check-certs =
@@ -121,8 +156,8 @@ in
           serviceConfig = {
             Type = "oneshot";
             Restart = "no";
-            StandardOutput = "journal";
-            StandardError = "journal";
+            StandardOutput = "console";
+            StandardError = "console";
             ExecStart = "${givcCertChecker}/bin/givc-check-certs";
           };
         };
