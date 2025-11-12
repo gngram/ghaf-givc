676d29be6f1c478208d436f4366bd2b8a992b938
Ganga Ram <Ganga.Ram@tii.ae>
2025-11-12 17:03:05 +0400

    open-policy-agent test
    Signed-off-by: Ganga Ram <Ganga.Ram@tii.ae>

--- nixos/tests/shared-configs.nix
+++ nixos/tests/shared-configs.nix
@@ -59,6 +59,15 @@ in
         inherit (adminConfig) name;
         inherit (adminConfig) addresses;
         tls.enable = tls;
+        open-policy-agent = {
+          enable = true;
+          policy = {
+            url = "http://github.com/gngram/policy-store/archive/refs/heads";
+            resource = "main.tar.gz";
+            sha256 = "sha256-dGfa/2/pfCsTd8Al6KFQdAz4d/roF+BBAuhbmbQs5JQ=";
+            updater.enable = false;
+          };
+        };
       };
     };
     tests-hostvm = {
